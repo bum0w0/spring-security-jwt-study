410f339a4f47c46758738ec4848491c350f69740
bum0w0 <kgy5988@naver.com>
2025-04-14 01:16:19 +0900

    회원가입 로직 구현

--- src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -21,9 +21,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         // CSRF disable
+        // CSRF는 세션 기반 인증에서 위험하지만, 현재는 JWT 인증 → 브라우저가 자동으로 쿠키를 전송하지 않으므로 CSRF 방어는 고려 X
         http.csrf((auth) -> auth.disable());
 
         // Form 로그인 방식 disable
+        // 기본 로그인 폼(Username/Password 입력창) 비활성화. REST API에서는 직접 클라이언트에서 로그인 데이터를 받아서 처리하므로 필요 없음
         http.formLogin((auth) -> auth.disable());
 
         // http basic 인증 방식 disable
