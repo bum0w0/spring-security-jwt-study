8a90111bc286944c7b5bc087cb1dbb3f336427d3
bum0w0 <kgy5988@naver.com>
2025-04-15 01:35:06 +0900

    커스텀 로그인 필터 구현

--- src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -1,16 +1,33 @@
 package com.example.springjwt.config;
 
+import com.example.springjwt.jwt.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    // AuthenticationManager가 사용할 AuthenticationConfiguration 객체를 생성자 주입받음
+    // 이 객체를 통해 Spring이 자동 구성한 AuthenticationManager를 가져올 수 있으며, 내부적으로 어떤 UserDetailsService와 PasswordEncoder를 사용할지에 대한 설정도 이미 포함되어 있음
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    // AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -36,6 +53,13 @@ public class SecurityConfig {
                 .requestMatchers("/login", "/", "/join").permitAll()
                 .requestMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated());
+
+
+        // UsernamePasswordAuthenticationFilter를 대체할 커스텀 필터(LoginFilter)를 등록하기 위해 addFilterAt() 사용
+        // LoginFilter는 인증 처리를 위해 AuthenticationManager를 사용하며, 이 매니저는 사용자 인증을 담당하는 객체
+        // AuthenticationManager는 UserDetailsService와 PasswordEncoder 등 인증에 필요한 설정을 포함하고 있으므로, 이를 생성자에 전달하여 필터가 인증을 처리할 수 있도록 설정
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         // 세션 설정
         http.sessionManagement((session) -> session
---
+++ src/main/java/com/example/springjwt/jwt/LoginFilter.java
@@ -0,0 +1,58 @@
+package com.example.springjwt.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+// 로그인 요청을 처리하는 커스텀 필터 (Form 로그인 방식은 사용하지 않음)
+public class LoginFilter extends UsernamePasswordAuthenticationFilter { // 이 필터는 기본적으로 "/login" 경로에 대한 POST 요청을 감지
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication( // 로그인 시도가 발생하면 실행되는 메소드
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws AuthenticationException {
+
+        // 클라이언트 요청에서 사용자의 username, password 추출
+        // 내부적으로 request.getParameter("username") 또는 request.getParameter("password")와 동일
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        /*
+        스프링 시큐리티에서 username과 password를 검증할 때는 Token에 담아서 AuthenticationManager로 전달하여 내부에서 검증 진행 (AuthenticationProvider가 검증)
+        이름은 Token이지만, Spring Security 내부에서 사용하는 인증 정보 객체라고 보면 됨 (Authentication 객체)
+        1. 인증 요청용 객체로써 사용 : 사용자가 로그인할 때, ID와 비밀번호를 담아서 인증 요청을 만들기 위해 사용
+        2. 인증 완료된 결과 객체로써 사용 : AuthenticationManager가 인증을 마친 후, UserDetails와 권한 정보를 담아 새로 생성한 인증 객체
+            → 인증 완료 시 이 객체가 SecurityContextHolder에 저장됨
+         */
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null); // 로그인 요청 시점에는 아직 인증이 되지 않은 상태이기 때문에, 사용자의 권한 정보가 없음. 인증이 완료되면 Authentication 객체 안에 실제 권한 정보가 채워짐 (Ex. User, Admin)
+
+        // AuthenticationManager로 전달
+        return authenticationManager.authenticate(authToken);
+    }
+
+    // authenticate()의 성공 여부가 로그인 성공/실패를 판별하는 기준, Spring Security가 그 결과에 따라 성공/실패 메서드를 자동으로 실행
+
+    // 로그인 성공 시 실행하는 메소드
+    // 매개변수에 filterchain : 인증 성공 후, 다음 필터로 요청을 넘길 수 있게 하기 위한 매개변수 (인증 성공 후에도 다른 필터에서 CORS 설정, 리스폰스 헤더 추가 등이 필요할 수 있음)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain, Authentication authentication) {
+
+    }
+
+    // 로그인 실패 시 실행하는 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+    }
+
+}
