d01797995a93ed35e8541a37451ca0c6688c8d6b
bum0w0 <kgy5988@naver.com>
2025-04-22 00:05:15 +0900

    CORS 설정

---
+++ src/main/java/com/example/springjwt/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.springjwt.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+
+}
